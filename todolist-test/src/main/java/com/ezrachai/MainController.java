ae3680b0f11181752527c36e8b491d2febd8e8b5
Ezra Chai <juanzhe2@gmail.com>
2025-10-27 12:21:19 +0800

    Implement TableView columns for Todo items in MainController and main.fxml

--- todolist-test/src/main/java/com/ezrachai/MainController.java
+++ todolist-test/src/main/java/com/ezrachai/MainController.java
@@ -1,19 +1,37 @@
 package com.ezrachai;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-public class MainController {
+public class MainController implements Initializable {
 
     @FXML
     private TableView<TodoItem> todoListTableView;
+    @FXML
+    private TableColumn<TodoItem, String> todoColumn;
+    @FXML
+    private TableColumn<TodoItem, String> descColumn;
+    @FXML
+    private TableColumn<TodoItem, String> categoryColumn;
+    @FXML
+    private TableColumn<TodoItem, String> priorityColumn;
+    @FXML
+    private TableColumn<TodoItem, LocalDate> dueDateColumn;
 
     public void openPopUp() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("assignment1.fxml"));
@@ -31,5 +49,14 @@ public class MainController {
 
     public void addTodo(TodoItem todoItem) {
         todoListTableView.getItems().add(todoItem);
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        todoColumn.setCellValueFactory(new PropertyValueFactory<>("todo"));
+        descColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
+        categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
+        priorityColumn.setCellValueFactory(new PropertyValueFactory<>("priority"));
+        dueDateColumn.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
     }
 }
